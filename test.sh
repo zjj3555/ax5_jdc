945823286fdd590ce056fecef7d5dbf4fbc9d2f7
zhishangweiji <2976294751@qq.com>
2025-04-06 16:35:54 +0800

    添加额外插件，修改tag

---
+++ test.sh
@@ -0,0 +1,21 @@
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+    branch="$1" repourl="$2" && shift 2
+    git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+    repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+    cd $repodir && git sparse-checkout set $@
+    mv -f $@ ../package
+    cd .. && rm -rf $repodir
+}
+
+#添加额外插件
+git clone --depth=1 https://github.com/mchome/luci-app-dogcom.git package/luci-app-dogcom
+
+
+# 科学上网插件
+git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
+git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
+git clone --depth=1 https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
